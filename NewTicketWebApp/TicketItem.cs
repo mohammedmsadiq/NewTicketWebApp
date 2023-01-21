0a7819eb2c5ae83092ab2adaaa3cb7a45278e0cb
Mohammed Sadiq <>
2023-01-21 21:34:44 +0400

    no message

---
+++ NewTicketWebApp/TicketItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace NewTicketWebApp
+{
+    public class TicketItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string LoggedDate { get; set; }
+        public bool Isdone { get; set; }
+    }
+}
+
---
+++ NewTicketWebApp/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data;
+
+namespace NewTicketWebApp
+{
+    public class User
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
+
